dbcd025191b1b256d696465df34bf0eaeded9c91
0227benson <benson3712297@gmail.com>
2024-04-06 10:10:18 +0800

    Add notes

--- src/main/java/com/example/api_jdbc/model/Student.java
+++ src/main/java/com/example/api_jdbc/model/Student.java
@@ -4,9 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Data//會自動生成 toString、equals、hashCode、getter、setter 方法
+@AllArgsConstructor//會生成一個包含所有屬性的constructor
+@NoArgsConstructor//會生成一個空的constructor
 public class Student {
     private int id;
     private String name;
@@ -14,5 +14,5 @@ public class Student {
 
     private String email;
 
-    private String phonenumber;/*定義屬性名稱和資料型態*/
+    private String phonenumber;//定義屬性名稱和資料型態
 }
